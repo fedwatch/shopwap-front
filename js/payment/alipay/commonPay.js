cc2f967984be1539e120a23a41f2edc302dc2479
yaodengying <yaodengying@yaodengying>
2017-05-09 09:51:06 +0800

    rrrrr

--- js/payment/alipay/commonPay.js
+++ js/payment/alipay/commonPay.js
@@ -27,6 +27,7 @@ define(function(require,exports,module){
         $(".checkPay").each(function(index,item){
             $(this).click(function(){
                $(this).find(".tick").addClass("tickSelected").parents().siblings().find(".tick").removeClass("tickSelected");
+               $(this).find(".morePay").css({visibility:"visible"}).parents().siblings().find(".morePay").css({visibility:"hidden"});
             })
         })
     });
