28bab3c579a8c6346733b2cedcf8bb369cd673a3
yaodengying <yaodengying@yaodengying>
2017-05-15 16:27:37 +0800

    rrrrr

--- js/my/address/address.js
+++ js/my/address/address.js
@@ -5,6 +5,76 @@ define(function(require,exports,module){
     require('user');
 
     $(function(){
+
+    });
+
+
+    require.async('handlebars',function(){
+        $.ajax({
+            url:BASE_URL+MEMBER_SITE_URL.LIST.URL,
+            type:MEMBER_SITE_URL.LIST.METHOD,
+            dataType:MEMBER_SITE_URL.DATATYPE,
+            data:{
+                username :username,
+                pageNumber  :pageNumber,
+            },
+            success:function (data) {
+                var tpl=require('/layout/my/address/address.tpl');
+                var template=Handlebars.compile(tpl);
+                var html=template(data);
+                $("#address").html(html);
+            }
+        });
+
+
+    });
+
+
+
+    var username = store.get("username");
+    var pageNumber = store.get("pageNumber");
+
+    require.async('handlebars',function(){
+        $.ajax({
+            url:BASE_URL+MEMBER_SITE_URL.LIST.URL,
+            type:MEMBER_SITE_URL.LIST.METHOD,
+            dataType:MEMBER_SITE_URL.DATATYPE,
+            data:{
+                username :username,
+                pageNumber  :pageNumber,
+            },
+            success:function (data) {
+                var tpl=require('/layout/my/address/manage-address.tpl');
+                var template=Handlebars.compile(tpl);
+                var html=template(data);
+                $("#manage-address").html(html);
+                edit();
+            }
+        });
+
+    });
+
+    /**
+     * 收货地址列表查看
+     * @param username
+     * @param pageNumber
+     */
+    function receiverList(username,pageNumber){
+        $.ajax({
+            url:BASE_URL+MEMBER_SITE_URL.LIST.URL,
+            type:MEMBER_SITE_URL.LIST.METHOD,
+            dataType:MEMBER_SITE_URL.DATATYPE,
+            data:{
+                username :username,
+                pageNumber  :pageNumber,
+            },
+            success:function (data) {
+                console.log(data);
+            }
+        });
+    }
+
+    function edit(){
         var $edit=$(".edit");
         var $addressOperate=$(".address-operate");
         var $addressList=$(".address-list").find(".ad-list");
@@ -29,26 +99,6 @@ define(function(require,exports,module){
                 $addressList.eq(index).remove();
             })
         });
-    });
+    }
 
-
-    require.async('handlebars',function(){
-        $.ajax({
-            url:BASE_URL+MEMBER_SITE_URL.LIST.URL,
-            type:MEMBER_SITE_URL.LIST.METHOD,
-            dataType:MEMBER_SITE_URL.DATATYPE,
-            data:{
-                username :username,
-                pageNumber  :pageNumber,
-            },
-            success:function (data) {
-                var tpl=require('/layout/my/address/address.tpl');
-                var template=Handlebars.compile(tpl);
-                var html=template(data);
-                $("#address").html(html);
-            }
-        });
-
-
-    });
 })
