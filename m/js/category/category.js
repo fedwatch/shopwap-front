1ca1eb201bbb62cd802019a7b5d64c73579793b7
jiangwangui <jwg@cnzqjt.com>
2017-06-05 09:16:48 +0800

    update

--- m/js/category/category.js
+++ m/js/category/category.js
@@ -16,8 +16,8 @@ define(function (require, exports, module) {
 
     $(function () {
 
-        var scroller = new IScroll('#productCategory', {  click: true,tap: true });
-        $('#productCategory').on('click, tap', '.productCategory a', function() {
+        var scroller = new IScroll('#productCategory', {click: true, tap: true});
+        $('#productCategory').on('click, tap', '.productCategory a', function () {
             //do something....
         });
 
@@ -39,7 +39,7 @@ define(function (require, exports, module) {
         /**
          *
          */
-        $(document).on('click','.productCategory a',function (e) {
+        $(document).on('click', '.productCategory a', function (e) {
             e.preventDefault();
             console.log($(this));
             getProductDetail($(this).data("id"));
@@ -49,7 +49,7 @@ define(function (require, exports, module) {
         /**
          * 主分类
          */
-        $(document).on('click','.nav-tabbar-item .nav-text',function(e){
+        $(document).on('click', '.nav-tabbar-item .nav-text', function (e) {
             // e.preventDefault();
             var $this = $(this);
             $("#categoryId").val($this.data("category-id"));
@@ -57,7 +57,7 @@ define(function (require, exports, module) {
             $(".nav-tabbar-item .nav-text").removeClass("active");
             $this.addClass("active");
             var tabbarBottomline = $("#tabbar-bottomline");
-            tabbarBottomline.css("transform","translate3d("+w+"px, 0px, 0px)");
+            tabbarBottomline.css("transform", "translate3d(" + w + "px, 0px, 0px)");
             var textLen = $(this).text().length;
             if (textLen == 4) {
                 tabbarBottomline.css("width", "5rem");
@@ -69,15 +69,15 @@ define(function (require, exports, module) {
 
             //点击获取二级菜单
             $.ajax({
-                url:BASE_URL+PRODUCT_SITE_URLS.FIND_SUBS.URL,
-                dataType:PRODUCT_SITE_URLS.DATATYPE,
-                type:PRODUCT_SITE_URLS.FIND_SUBS.METHOD,
-                data:{username:username,id:$("#categoryId").val()},
-                async:false,
-                cache:true,
-                success:function (data) {
+                url: BASE_URL + PRODUCT_SITE_URLS.FIND_SUBS.URL,
+                dataType: PRODUCT_SITE_URLS.DATATYPE,
+                type: PRODUCT_SITE_URLS.FIND_SUBS.METHOD,
+                data: {username: username, id: $("#categoryId").val()},
+                async: false,
+                cache: true,
+                success: function (data) {
                     // navSubPosition
-                    require.async('handlebars',function(){
+                    require.async('handlebars', function () {
                         var getData = data;
                         var tpl = require('/m/layout/cartgory/navSubPosition.tpl');
                         var template = Handlebars.compile(tpl);
@@ -110,7 +110,7 @@ define(function (require, exports, module) {
         });
 
         //二级分类
-        $(document).on('click','.nav-sub-tabbar-item .nav-sub-text',function (e) {
+        $(document).on('click', '.nav-sub-tabbar-item .nav-sub-text', function (e) {
             var $this = $(this);
             $(".nav-sub-render .nav-sub-text").removeClass("active");
             $this.addClass("active");
@@ -126,112 +126,111 @@ define(function (require, exports, module) {
         });
     });
 
-    function getProductDetail(id){
+    function getProductDetail(id) {
         var productId = id;
-        store.set("currentProductID",productId);
+        store.set("currentProductID", productId);
         location.href = '/m/html/detail/detail.html';
     }
 
 //商品查询1
- function getProductInfo(){
+    function getProductInfo() {
         var categoryIdsHidden = $("#categoryId").val();
         var categorySubId = $("#categorySubId").val();
         var keyword = '';
         var pageNumber = '';
-        var categoryIds = categoryIdsHidden+","+categorySubId;
+        var categoryIds = categoryIdsHidden + "," + categorySubId;
         var brandIds = '';
         var startPrice = '';
         var endPrice = '';
         var pageSize = '';
 
-     var pageOp = $("#productCategory").page({
-         url:BASE_URL+PRODUCT_SITE_URLS.PRODUCT_SEARCH.URL,
-         param:{},
-         processResult:function(result){
-             var html = "";
-             if(result.products != null){
-                 $.each(result.products, function(index, data){
-                     html +='<div class="col-50 productCategory" >'+
-                         '<a href="javascript:;"  data-id="'+data.id+'" class="external">'+
-                         '<div style="background: #fff;padding:0 0;margin: .25rem 0;width:100%;height:12rem;overflow: hidden;">'+
-                         '<img src="'+data.image+'" alt="" style="width:100%;height:7rem;overflow: hidden;">'+
-                         '<h4 class="product-title" style="padding:0 .25rem;font-size:.7rem;color:#000;">'+data.name+'</h4>'+
-                         '<span class="product-price-box" style="padding:0 .25rem;font-size:.5rem;color:#ff0000;">￥<span style="font-size:.65rem;">'+data.price+'</span>'+'</span>'+'<span class="product-buyer-number-box" style="font-size:.5rem;color:#999;">已有 <span>'+data.sales+'</span>人购买</span>'+ '</div>'+
-                         '</a>'+ '</div>';
-                 });
+        var pageOp = $("#productCategory").page({
+            url: BASE_URL + PRODUCT_SITE_URLS.PRODUCT_SEARCH.URL,
+            param: {},
+            processResult: function (result) {
+                var html = "";
+                if (result.products != null) {
+                    $.each(result.products, function (index, data) {
+                        html += '<div class="col-50 productCategory" >' +
+                            '<a href="javascript:;"  data-id="' + data.id + '" class="external">' +
+                            '<div style="background: #fff;padding:0 0;margin: .25rem 0;width:100%;height:12rem;overflow: hidden;">' +
+                            '<img src="' + data.image + '" alt="" style="width:100%;height:7rem;overflow: hidden;">' +
+                            '<h4 class="product-title" style="padding:0 .25rem;font-size:.7rem;color:#000;">' + data.name + '</h4>' +
+                            '<span class="product-price-box" style="padding:0 .25rem;font-size:.5rem;color:#ff0000;">￥<span style="font-size:.65rem;">' + data.price + '</span>' + '</span>' + '<span class="product-buyer-number-box" style="font-size:.5rem;color:#999;">已有 <span>' + data.sales + '</span>人购买</span>' + '</div>' +
+                            '</a>' + '</div>';
+                    });
 
-             }
+                }
 
-             return html;
-         }
-     });
-     pageOp.load({categoryIds:categoryIds});
-
-/*        $.ajax({
-            url:BASE_URL+PRODUCT_SITE_URLS.PRODUCT_SEARCH.URL,
-            dataType:PRODUCT_SITE_URLS.DATATYPE,
-            type:PRODUCT_SITE_URLS.PRODUCT_SEARCH.METHOD,
-            data: {
-                keyword: keyword,
-                pageNumber: pageNumber,
-                categoryIds: categoryIds,
-                brandIds: brandIds,
-                startPrice: startPrice,
-                endPrice: endPrice,
-                pageSize: pageSize
-            },
-            async:false,
-            success:function (data) {
-                // productCategory
-                require.async('handlebars',function(){
-                    if(data.authStatus == "200"){
-                        var tpl = require('/m/layout/cartgory/productCategory.tpl');
-                        var template = Handlebars.compile(tpl);
-                        var html = template(data);
-                        $("#productCategory").html(html);
-                    }
-                });
+                return html;
             }
-        });*/
+        });
+        pageOp.load({categoryIds: categoryIds});
+
+        /*        $.ajax({
+         url:BASE_URL+PRODUCT_SITE_URLS.PRODUCT_SEARCH.URL,
+         dataType:PRODUCT_SITE_URLS.DATATYPE,
+         type:PRODUCT_SITE_URLS.PRODUCT_SEARCH.METHOD,
+         data: {
+         keyword: keyword,
+         pageNumber: pageNumber,
+         categoryIds: categoryIds,
+         brandIds: brandIds,
+         startPrice: startPrice,
+         endPrice: endPrice,
+         pageSize: pageSize
+         },
+         async:false,
+         success:function (data) {
+         // productCategory
+         require.async('handlebars',function(){
+         if(data.authStatus == "200"){
+         var tpl = require('/m/layout/cartgory/productCategory.tpl');
+         var template = Handlebars.compile(tpl);
+         var html = template(data);
+         $("#productCategory").html(html);
+         }
+         });
+         }
+         });*/
     }
 
 
-
-    function initCategoryActive(){
+    function initCategoryActive() {
         $(".nav-tabbar-item").first().find(".nav-render > .nav-text").addClass("active");
         $(".nav-sub-text").first().addClass("active");
     }
 
 
     // mainNav
-    require.async('handlebars',function(){
+    require.async('handlebars', function () {
         $.ajax({
-            url:BASE_URL+PRODUCT_SITE_URLS.FIND_ROOTS.URL,
-            type:PRODUCT_SITE_URLS.FIND_ROOTS.METHOD,
-            dataType:"json",
-            data:{username:username},
-            cache:true,
-            async:false,
-            success:function (data) {
+            url: BASE_URL + PRODUCT_SITE_URLS.FIND_ROOTS.URL,
+            type: PRODUCT_SITE_URLS.FIND_ROOTS.METHOD,
+            dataType: "json",
+            data: {username: username},
+            cache: true,
+            async: false,
+            success: function (data) {
 
-                if(data.authStatus == "200"){
+                if (data.authStatus == "200") {
                     var tpl = require('/m/layout/cartgory/mainNav.tpl');
                     var template = Handlebars.compile(tpl);
                     var html = template(data);
                     $("#mainNav").html(html);
 
                     // navSubPosition
-                    require.async('handlebars',function(){
-                        var categoryId = data.productCategories[0].id ;
+                    require.async('handlebars', function () {
+                        var categoryId = data.productCategories[0].id;
                         $.ajax({
-                            url:BASE_URL+PRODUCT_SITE_URLS.FIND_SUBS.URL,
-                            type:PRODUCT_SITE_URLS.FIND_SUBS.METHOD,
-                            data:{username:username,id:categoryId},
-                            dataType:PRODUCT_SITE_URLS.DATATYPE,
-                            cache:true,
-                            async:false,
-                            success:function (data) {
-                                if(data.authStatus == '200'){
+                            url: BASE_URL + PRODUCT_SITE_URLS.FIND_SUBS.URL,
+                            type: PRODUCT_SITE_URLS.FIND_SUBS.METHOD,
+                            data: {username: username, id: categoryId},
+                            dataType: PRODUCT_SITE_URLS.DATATYPE,
+                            cache: true,
+                            async: false,
+                            success: function (data) {
+                                if (data.authStatus == '200') {
                                     var tpl = require('/m/layout/cartgory/navSubPosition.tpl');
                                     var template = Handlebars.compile(tpl);
                                     var html = template(data);
@@ -241,12 +240,12 @@ define(function (require, exports, module) {
                                     var categorySubId = data.productCategories[0].id;
                                     var keyword = '';
                                     var pageNumber = '1';
-                                    var categoryIds = categoryIdsHidden+","+categorySubId;
+                                    var categoryIds = categoryIdsHidden + "," + categorySubId;
                                     var brandIds = '';
                                     var startPrice = '';
                                     var endPrice = '';
                                     var pageSize = '20';
-                                    getSearchResults(keyword,pageNumber,categoryIds,brandIds,startPrice,endPrice,pageSize,'productCategory');
+                                    getSearchResults(keyword, pageNumber, categoryIds, brandIds, startPrice, endPrice, pageSize, 'productCategory');
                                 }
                             }
                         });
@@ -259,70 +258,71 @@ define(function (require, exports, module) {
     });
 
     //商品查询
-   function getSearchResults(keyword,pageNumber,categoryIds,brandIds,startPrice,endPrice,pageSize,DOM){
-       var pageOp = $("#productCategory").page({
-           url:BASE_URL+PRODUCT_SITE_URLS.PRODUCT_SEARCH.URL,
-           param:{},
-           processResult:function(result){
-               var html = "";
-               if(result.products != null){
-                   $.each(result.products, function(index, data){
-                       html +='<div class="col-50 productCategory" >'+
-                           '<a href="javascript:;"  data-id="'+data.id+'" class="external">'+
-                           '<div style="background: #fff;padding:0 0;margin: .25rem 0;width:100%;height:12rem;overflow: hidden;">'+
-                           '<img src="'+data.image+'" alt="" style="width:100%;height:7rem;overflow: hidden;">'+
-                           '<h4 class="product-title" style="padding:0 .25rem;font-size:.7rem;color:#000;">'+data.name+'</h4>'+
-                           '<span class="product-price-box" style="padding:0 .25rem;font-size:.5rem;color:#ff0000;">￥<span style="font-size:.65rem;">'+data.price+'</span>'+'</span>'+'<span class="product-buyer-number-box" style="font-size:.5rem;color:#999;">已有 <span>'+data.sales+'</span>人购买</span>'+ '</div>'+
-                           '</a>'+ '</div>';
-                   });
+    function getSearchResults(keyword, pageNumber, categoryIds, brandIds, startPrice, endPrice, pageSize, DOM) {
+        var pageOp = $("#productCategory").page({
+            url: BASE_URL + PRODUCT_SITE_URLS.PRODUCT_SEARCH.URL,
+            param: {},
+            processResult: function (result) {
+                var html = "";
+                if (result.products != null) {
+                    $.each(result.products, function (index, data) {
+                        html += '<div class="col-50 productCategory" >' +
+                            '<a href="javascript:;"  data-id="' + data.id + '" class="external">' +
+                            '<div style="background: #fff;padding:0 0;margin: .25rem 0;width:100%;height:12rem;overflow: hidden;">' +
+                            '<img src="' + data.image + '" alt="" style="width:100%;height:7rem;overflow: hidden;">' +
+                            '<h4 class="product-title" style="padding:0 .25rem;font-size:.7rem;color:#000;">' + data.name + '</h4>' +
+                            '<span class="product-price-box" style="padding:0 .25rem;font-size:.5rem;color:#ff0000;">￥<span style="font-size:.65rem;">' + data.price + '</span>' + '</span>' + '<span class="product-buyer-number-box" style="font-size:.5rem;color:#999;">已有 <span>' + data.sales + '</span>人购买</span>' + '</div>' +
+                            '</a>' + '</div>';
+                    });
 
-               }
+                }
 
-               return html;
-           }
-       });
-
-       pageOp.load({
-           keyword: keyword,
-           pageNumber: pageNumber,
-           categoryIds: categoryIds,
-           brandIds: brandIds,
-           startPrice: startPrice,
-           endPrice: endPrice,
-           pageSize: pageSize});
-
-/*        $.ajax({
-            url:BASE_URL+PRODUCT_SITE_URLS.PRODUCT_SEARCH.URL,
-            dataType:PRODUCT_SITE_URLS.DATATYPE,
-            type:PRODUCT_SITE_URLS.PRODUCT_SEARCH.METHOD,
-            cache:true,
-            async:false,
-            data: {
-                keyword: keyword,
-                pageNumber: pageNumber,
-                categoryIds: categoryIds,
-                brandIds: brandIds,
-                startPrice: startPrice,
-                endPrice: endPrice,
-                pageSize: pageSize
-            },
-            success:function (data) {
-                // productCategory
-                require.async('handlebars',function(){
-                    if(data.authStatus == "200"){
-                        var tpl = require('/m/layout/cartgory/productCategory.tpl');
-                        var template = Handlebars.compile(tpl);
-                        var html = template(data);
-                        $("#"+DOM).html(html);
-                    }
-                });
-               // $($(".nav-sub-render .nav-sub-text")[0]).addClass("active");
+                return html;
             }
-        });*/
+        });
+
+        pageOp.load({
+            keyword: keyword,
+            pageNumber: pageNumber,
+            categoryIds: categoryIds,
+            brandIds: brandIds,
+            startPrice: startPrice,
+            endPrice: endPrice,
+            pageSize: pageSize
+        });
+
+        /*        $.ajax({
+         url:BASE_URL+PRODUCT_SITE_URLS.PRODUCT_SEARCH.URL,
+         dataType:PRODUCT_SITE_URLS.DATATYPE,
+         type:PRODUCT_SITE_URLS.PRODUCT_SEARCH.METHOD,
+         cache:true,
+         async:false,
+         data: {
+         keyword: keyword,
+         pageNumber: pageNumber,
+         categoryIds: categoryIds,
+         brandIds: brandIds,
+         startPrice: startPrice,
+         endPrice: endPrice,
+         pageSize: pageSize
+         },
+         success:function (data) {
+         // productCategory
+         require.async('handlebars',function(){
+         if(data.authStatus == "200"){
+         var tpl = require('/m/layout/cartgory/productCategory.tpl');
+         var template = Handlebars.compile(tpl);
+         var html = template(data);
+         $("#"+DOM).html(html);
+         }
+         });
+         // $($(".nav-sub-render .nav-sub-text")[0]).addClass("active");
+         }
+         });*/
     }
 
     // footerNav
-    require.async('handlebars',function(){
+    require.async('handlebars', function () {
         var data = {};
         var tpl = require('/m/layout/common/footerBar.tpl');
         var template = Handlebars.compile(tpl);
