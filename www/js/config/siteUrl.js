646c077722ce27481f8985ffb7618106af0ea1a1
jiangwangui <jwg@cnzqjt.com>
2017-06-13 10:13:02 +0800

    update

--- www/js/config/siteUrl.js
+++ www/js/config/siteUrl.js
@@ -3,8 +3,8 @@
  */
 //基本URL
 // var BASE_URL = "http://"+location.host+"/wap";
-// var BASE_URL = "http://192.168.3.70:8888/wap";
-var BASE_URL = "https://jie.net/wap";
+var BASE_URL = "http://192.168.3.70:8888/wap";
+// var BASE_URL = "https://jie.net/wap";
 // var BASE_URL = "http://60.205.84.124/wap";
 
 
